846cdab1ce54139f7ce7dc0a5307f0ad9413b69e
Cuong Nguyen <cuongbet@gmail.com>
2020-06-03 00:27:41 +0700

    Livani Store

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   $('.main-carousel .carousel').carousel({
+       interval:2000
+   }); 
+
+   $('.product-area .product-list .product-image .overlay i.fa').tooltip();
+});
