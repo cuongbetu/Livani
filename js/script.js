8a32432602d5d80a7a1c2ebf22f0371252736ef4
Cuong Nguyen <cuongbet@gmail.com>
2020-06-06 16:53:49 +0700

    80% cua Home Page

--- js/script.js
+++ js/script.js
@@ -4,4 +4,68 @@ $(document).ready(function() {
    }); 
 
    $('.product-area .product-list .product-image .overlay i.fa').tooltip();
+
+   $('.our-partners-slide').slick({
+       arrows : false,
+       slidesToShow : 6,
+       slidesToScroll : 2,
+       autoplay : true,
+       pauseOnHover : false,
+       autolaySpeed : 3000,
+       responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }
+      ]
+   });
+
+   $('.facility-slide').slick({
+       arrows : false,
+       slidesToShow : 4,
+       slidesToScroll : 1,
+       pauseOnHover : false,
+       autoplay : true,
+       autolaySpeed : 3000,
+       responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }
+      ]
+   });
 });
